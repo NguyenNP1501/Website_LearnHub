1f2e0e37702b4a16698b88c732d83e3383491dc9
trancongdanh044-ui <trancongdanh044@gmail.com>
2026-04-28 15:39:51 +0700

    Create CreateExam.jsx

---
+++ frontend/src/pages/admin/CreateExam.jsx
@@ -0,0 +1,23 @@
+import { useNavigate } from "react-router-dom";
+import { createExam } from "../../services/examApi";
+import ExamForm from "../../components/ExamForm/ExamForm";
+import GoBack from "../../components/GoBack/GoBack";
+
+function CreateExam() {
+  const navigate = useNavigate();
+
+  const handleCreate = async (data) => {
+    await createExam(data);
+    navigate("/admin");
+  };
+
+  return (
+    <div>
+      <div className="page">Trang tạo đề thi thủ công</div>
+      <GoBack></GoBack>
+      <ExamForm onSubmit={handleCreate} />
+    </div>
+  );
+}
+
+export default CreateExam;
