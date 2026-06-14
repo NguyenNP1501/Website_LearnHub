46b0f265c38df8629e4f26f10319ff4facd11d0e
trancongdanh044-ui <trancongdanh044@gmail.com>
2026-06-14 11:34:52 +0700

    Thêm controller lấy bài viết, search bài viết

--- backend/controller/getPost.js
+++ backend/controller/getPost.js
@@ -10,3 +10,35 @@ exports.getPosts = async (req, res) => {
         res.status(500).json({ error: 'Internal server error' });
     }
 }
+
+exports.getNearestPost = async (req, res, next) =>{
+    try{
+        const nearestPosts = await Post.getNearestPost();
+        if(!nearestPosts){
+            res.status(404).json({
+                message: "Không tìm thấy bài đăng",
+            })
+        }
+        res.status(200).json(nearestPosts)
+    }catch(error){
+        next(error);
+    }
+}
+
+exports.searchNearestPost = async (req, res, next) =>{
+    try{
+        const keyword = req.query.keyword;
+        const nearestPostList = await Post.searchNearestPost(String(keyword));
+
+        if(!keyword && !nearestPostList){
+            res.status(404).json({
+                message: "Không tìm thầy bài viết"
+            })
+        }
+        else {
+            res.status(200).json(nearestPostList);
+        }
+    }catch(error){
+        next(error);
+    }
+}
