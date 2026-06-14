b0b62b68bb659f5c28c9fac406d29a841dcb2279
trancongdanh044-ui <trancongdanh044@gmail.com>
2026-06-14 11:48:38 +0700

    Enhance Home component with API data fetching
    Refactor Home component to fetch latest posts and courses from APIs. Implement search functionality for posts based on user input.

--- frontend/src/components/Home/Home.jsx
+++ frontend/src/components/Home/Home.jsx
@@ -1,82 +1,143 @@
-import { useNavigate } from 'react-router-dom';
+import { Link} from 'react-router-dom';
 import { GraduationCap } from 'lucide-react';
 import '../../App.css'; 
 import './Home.css';
+import { useState } from 'react';
+import { useEffect } from 'react';
 
 export default function Home() {
-  const navigate = useNavigate();
 
-  // Dữ liệu giả lập cho 4 khóa học bên cột phải
-  const featuredCourses = [1, 2, 3, 4];
+  const [latestPost, setLatestPost] = useState([]);
+  const [latestCourse, setLatestCourse] = useState([]);
+  const [keyword, setKeyword] = useState(null);
+  const [searchResult, setSearchResult] = useState([]);
 
+  const LATEST_POST_API_URL = "http://localhost:3000/api/nearestposts";
+  const LATEST_COURSE_API_URL = "http://localhost:3000/api/latestcourses";
+  const SEARCH_POST_APT_URL = "http://localhost:3000/api/nearestposts/search?keyword="
+  const fetchAPI = async (API_URL) =>{
+    const response = await fetch(API_URL);
+    const result = await response.json();
+    return result;
+  }
+  useEffect(() =>{
+    const loadData = async () =>{
+      const data = await fetchAPI(LATEST_POST_API_URL);
+      setLatestPost(data);
+    }
+    loadData();
+  }, [])
+
+  useEffect(() =>{
+    const loadData = async () =>{
+      const data = await fetchAPI(LATEST_POST_API_URL);
+      setLatestCourse(data);
+    }
+    loadData();
+  }, [])
+  
+  useEffect(()=>{
+      const loadData = async () =>{
+        const data = await fetchAPI(SEARCH_POST_APT_URL + `${keyword}`);
+        setSearchResult(data);
+      }
+      loadData();
+  }, [keyword]);
+
+  
   return (
     <div className="home-page-container">
       
       {/* CỘT TRÁI: DANH SÁCH BÀI VIẾT */}
       <div className="left-column">
-        <h2 className="section-title">Bài viết nổi bật</h2>
-        
-        {/* Bài viết 1 */}
-        <div className="article-card">
-          <h3 className="article-title">Bộ giáo dục thông tin về kỳ thi THPT 2026</h3>
-          <p className="article-snippet">Bộ giáo dục đã thông báo...</p>
-          
-          <div className="article-divider-wrapper">
-            <span className="article-author">Tác giả: Teacher A...</span>
-          </div>
-          
-          <button className="btn-article">Xem bài viết</button>
-        </div>
+        <h2 className="section-title">Bài viết mới nhất</h2>
 
-        {/* Bài viết 2 */}
-        <div className="article-card">
-          <h3 className="article-title">Bộ giáo dục thông tin về kỳ thi THPT 2026</h3>
-          <p className="article-snippet">Bộ giáo dục đã thông báo...</p>
-          
-          <div className="article-divider-wrapper">
-            <span className="article-author">Tác giả: Teacher A...</span>
+        {latestPost.map((item, index) => (
+          <div className="article-card" key={index}>
+            <h3 className="article-title">{item.title}</h3>
+            {/* <p className="article-snippet">{item.content.slice(0, 100) ? item.content.length() > 100 : item.content}</p> */}
+            <p className="article-snippet">{item.content}</p>
+            
+            <div className="article-divider-wrapper">
+              <span className="article-author">{item.user_name}</span>
+            </div>
+
+            <Link to={`/view-post/${item.post_id}`}>
+              <button className="btn-article">Xem bài viết</button>
+            </Link>
           </div>
-          
-          <button className="btn-article">Xem bài viết</button>
-        </div>
+        ))}
+        
       </div>
 
       {/* CỘT PHẢI: TÌM KIẾM VÀ KHÓA HỌC */}
       <div className="right-column">
         
         {/* Khu vực Tìm kiếm */}
-        <div className="search-section">
+        {/* <div className="search-section">
           <h2 className="section-title">Tìm bài viết</h2>
           <input 
             type="text" 
             placeholder="Search..." 
             className="search-input"
+            onChange={handleSearch}
+            onKeyDown={(e) => {
+              if(e.key === "Enter"){
+                handleSearch(e);
+              }
+            }}
           />
-        </div>
+        </div> */}
+
+        {/* {
+          (searchResult.length === 0 && (keyword === "" || keyword === null)) 
+          ? <p>Không tìm thấy bài viết</p> 
+          : <div className="featured-courses-section">
+              <h2 className="section-title">Kết quả tìm kiếm</h2>
+              <div className="featured-courses-grid">
+                {searchResult.map((item, index) => (
+                  <div key={index} className="course-card-mini">
+                    <div className="course-icon-wrapper">
+                      <img src={item.img_url} />
+                    </div>
+                    <div className="course-card-mini-content">
+                      <div className="course-card-mini-title">{item.course_name}</div>
+                      <Link to={`course/${item.course_id}`}>
+                        <button 
+                        className="btn-course-mini"
+                        >
+                          Học ngay
+                      </button>
+                      </Link>
+                    </div>
+                  </div>
+                ))}
+            </div>
+          </div>
+        } */}
 
         {/* Khu vực Khóa học nổi bật */}
         <div className="featured-courses-section">
-          <h2 className="section-title">Khoá học nổi bật</h2>
+          <h2 className="section-title">Khoá học mới nhất</h2>
           <div className="featured-courses-grid">
             
-            {featuredCourses.map((item) => (
-              <div key={item} className="course-card-mini">
+            {latestCourse.map((item, index) => (
+              <div key={index} className="course-card-mini">
                 <div className="course-icon-wrapper">
-                  <GraduationCap size={65} color="#2563eb" strokeWidth={1.5} />
+                  <img src={item.img_url} />
                 </div>
                 <div className="course-card-mini-content">
-                  <div className="course-card-mini-title">Toán 12</div>
-                  <button 
+                  <div className="course-card-mini-title">{item.course_name}</div>
+                  <Link to={`course/${item.course_id}`}>
+                    <button 
                     className="btn-course-mini"
-                    // Chuyển hướng tạm về khóa học có ID là 1
-                    onClick={() => navigate('/course/1')} 
-                  >
-                    Học ngay
+                    >
+                      Học ngay
                   </button>
+                  </Link>
                 </div>
               </div>
             ))}
-
           </div>
         </div>
         
